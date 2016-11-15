e17baf3ff6a986bd29b9c8cc73f836e7b8b5bfb8
Scott Neaves <scottneaves2014@u.northwestern.edu>
2016-11-15 10:42:50 -0600

    start using gunicorn server

--- app.py
+++ app.py
@@ -44,5 +44,4 @@ def retrieve(uid):
         else:
             return json.dumps({'error': 'The unique ID in the URL doesn\'t match any saved cURL commands.'})
 
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+app.run()
